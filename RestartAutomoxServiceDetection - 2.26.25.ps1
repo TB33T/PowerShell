2680762f1d7d2508beb4443d1645e2d59c47532f
TB33T <brunnertrevor@gmail.com>
2025-04-25 13:38:35 -0600

    Add files via upload

---
+++ RestartAutomoxServiceDetection - 2.26.25.ps1
@@ -0,0 +1,25 @@
+﻿#Script to check if the Automox Agent(amagent) service is running
+#If Exit 1 runs the Remediation script will be run
+# Detection Script
+
+# Replace "YourServiceName" with the actual service name
+$ServiceName = "amagent"
+
+$Service = Get-Service -Name $ServiceName -ErrorAction SilentlyContinue
+
+if($Service -eq $null) 
+{
+    Write-Host "Service '$ServiceName' not found."
+    exit 1 # Not found, therefore not compliant.
+}
+
+if($Service.Status -ne "Running") 
+{
+    Write-Host "Service '$ServiceName' is not running."
+    exit 1 # Not running, therefore not compliant. Will run the remediation script.
+} 
+else 
+{
+    Write-Host "Service '$ServiceName' is running."
+    exit 0 # Running, therefore compliant.
+}
---
+++ RestartAutomoxServiceRemediation - 2.26.25.ps1
@@ -0,0 +1,35 @@
+﻿#Script to start the Automox Agent(amagent) service
+
+# Remediation Script
+
+# Replace "YourServiceName" with the actual service name
+$ServiceName = "amagent"
+
+# Check if the service is running
+$Service = Get-Service -Name $ServiceName -ErrorAction SilentlyContinue
+
+if($Service -eq $null) 
+{
+    Write-Warning "Service '$ServiceName' not found."
+    exit 1 # Indicate failure
+}
+
+if($Service.Status -ne "Running") 
+{
+    try 
+    {
+        Start-Service -Name $ServiceName -ErrorAction Stop
+        Write-Host "Service '$ServiceName' started successfully."
+        exit 0 # Indicate success
+    }
+    catch 
+    {
+        Write-Error "Failed to start service '$ServiceName': $($_.Exception.Message)"
+        exit 2 # Indicate failure
+    }
+} 
+else 
+{
+    Write-Host "Service '$ServiceName' is already running."
+    exit 0 # Indicate success
+}
