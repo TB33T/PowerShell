bd30e8ad2f68c4660817da19c48aa6800cda4abc
TB33T <brunnertrevor@gmail.com>
2025-04-25 13:45:46 -0600

    Add files via upload

---
+++ DeleteDeviceLock.ps1
@@ -0,0 +1,9 @@
+﻿#script to check registry for DeviceLock and delete if it exists
+$path = "HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\DeviceLock"
+
+#check if the path exists
+if ((Get-Item -Path $path -ErrorAction Ignore) -ne $null) 
+{
+    #Remove the value
+    Remove-Item -Path $path -ErrorAction Ignore -Recurse -Force
+}
